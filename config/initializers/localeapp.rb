21c795ecbe6597d854aac5563b67052a56311172
Amaia Castro <amaia@amaiac.net>
2015-07-06 12:20:50 +0200

    Remove localeapp

--- config/initializers/localeapp.rb
+++
@@ -1,8 +0,0 @@
-require 'localeapp/rails'
-
-Localeapp.configure do |config|
-  config.api_key = Rails.application.secrets["localeapp_apikey"]
-  config.sending_environments = []
-  config.reloading_environments = []
-  config.cache_missing_translations = true
-end
