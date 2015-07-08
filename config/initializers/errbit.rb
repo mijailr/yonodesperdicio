df4dfba351894352370ba61836aa42418fa024f6
Andrés Pereira <apereira@alabs.es>
2015-07-08 12:18:21 +0200

    [deploy] errbit/airbrake error tracking

---
+++ config/initializers/errbit.rb
@@ -0,0 +1,6 @@
+Airbrake.configure do |config|
+  config.api_key = Rails.application.secrets.airbrake["apikey"]
+  config.host    = Rails.application.secrets.airbrake["host"]
+  config.port    = Rails.application.secrets.airbrake["port"]
+  config.secure  = config.port == 443
+end 
