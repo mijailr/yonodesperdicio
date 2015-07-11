5a89c8741569ad21450f9ce26fa30cc65d315da8
Amaia Castro <amaia@amaiac.net>
2015-07-11 19:27:39 +0200

    Admin for Articles and Ideas

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,9 @@ require 'sidekiq/web'
 
 Yonodesperdicio::Application.routes.draw do
 
+  devise_for :admin_users, ActiveAdmin::Devise.config
+  ActiveAdmin.routes(self)
+  
   root 'home#index'
 
   scope '/api' do
