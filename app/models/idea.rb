8bc908cbd212ac105d3b5be7710d92af51529108
AnaTecla <anatecla333@gmail.com>
2015-07-24 15:27:21 +0200

    Un poco más de diseño

--- app/models/idea.rb
+++ app/models/idea.rb
@@ -10,7 +10,7 @@ class Idea < ActiveRecord::Base
     self.published_at = Time.now
   end
 
-  has_attached_file :image, styles: {thumb: "100x100>", medium: "400x230#", large: "530x300>"}
+  has_attached_file :image, styles: {thumb: "100x100>", mediumidea: "400x230#", large: "530x300>"}
   validates_attachment :image, content_type: { content_type: ["image/jpg", "image/jpeg", "image/png", "image/gif"] }
   validates_attachment_size :image, :in => 0.megabytes..1.megabytes
 end
