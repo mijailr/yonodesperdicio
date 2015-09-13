2fa6b7ede6cfea14840266d356a4a2dd7c6b1615
Amaia Castro <amaia@amaiac.net>
2015-09-13 13:16:13 +0200

    Sendgrid config

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,12 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the Rails application.
 Yonodesperdicio::Application.initialize!
+
+ActionMailer::Base.smtp_settings = {
+  :user_name => Rails.application.secrets.emails["smtp_username"],
+  :password => Rails.application.secrets.emails["smtp_password"],
+  :address => 'smtp.sendgrid.net',
+  :port => 587,
+  :authentication => :plain,
+  :enable_starttls_auto => true
+}
