0ecbce19578f2b257c499b02e2201e9763e01955
Amaia Castro <amaia@amaiac.net>
2015-09-13 18:35:38 +0200

    fix permitted params

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -14,10 +14,10 @@ class RegistrationsController < Devise::RegistrationsController
   private
 
   def sign_up_params
-    params.require(:user).permit(:name, :user_name, :email, :password, :password_confirmation, :zipcode, :city, :province)
+    params.require(:user).permit(:name, :username, :email, :password, :password_confirmation, :zipcode, :city, :province)
   end
 
   def account_update_params
-    params.require(:user).permit(:name, :user_name, :email, :password, :password_confirmation, :current_password, :zipcode, :city, :province, :image)
+    params.require(:user).permit(:name, :username, :email, :password, :password_confirmation, :current_password, :zipcode, :city, :province, :image)
   end
 end
