18d8f760ac71560d95633dcd92880e42f9706d5d
AnaTecla <anatecla333@gmail.com>
2015-09-15 10:32:50 +0200

    Cosas de diseño

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,10 +8,133 @@
 
 Text.create code: "faq", 
             title: "Preguntas frecuentes", 
-            body: "lalala"
+            body: "### 1. ¿Qué es yonodesperdicio.org?
+*Yonodesperdicio.org* es una iniciativa de Prosalus para reducir el desperdicio de alimentos, principalmente en los hogares. *Yonodesperdicio.org* quiere ser una red ciudadana comprometida con la reducción del desperdicio de alimentos; ofrece recursos contra el desperdicio y pone en contacto con otras personas que quieren entregar alimentos (o necesitan alimentos). 
+
+### 2. ¿Qué se comparte?
+Se comparten ÚNICAMENTE alimentos. Para otro tipo de artículos, tienes otras páginas. Desde aquí te recomendamos <a href="http://nolotiro.org" target="_blank">*nolotiro.org*</a> para buscar y ofrecer otros artículos de segunda mano, de forma gratuita.
+
+También puedes compartir recetas o trucos para reducir el desperdicio de alimentos en el apartado de IDEAS FRESCAS;  te invitamos a hacerlo.
+
+### 3. ¿Por qué compartir alimentos?
+Se trata de alimentos que podrían acabar en la basura si no los consumimos a tiempo. La iniciativa quiere poner en marcha una red que facilite la entrega de alimentos a otras personas o asociaciones y la **reducción del desperdicio** de los mismos. 
+
+### 4. ¿Qué es lo que NO se puede compartir?
+Nunca alimentos caducados o en mal estado, ni medicamentos. Tampoco se puede compartir ropa u otros enseres, para eso, como te hemos comentado, hay otras páginas.
+
+### 5. ¿Cómo es el tipo de intercambio? 
+No es una página comercial ni de ventas: lo entregas gratis, lo regalas, no se contemplan transacciones económicas. Si tú entregas comida a otra persona y evitamos que vaya a la basura, TODAS LAS PERSONAS GANAMOS. Ese es nuestro beneficio.
+
+### 6. ¿Cómo creo un usuario o usuaria?
+En la web, en el apartado de “Regístrate” deberás introducir tus datos para darte de alta como usuario/a (nombre y apellidos, nombre de usuario/a, contraseña, municipio y provincia de residencia y el código postal, además de un email de contacto). Una vez hecho el registro, podrás editar tu perfil colocando una foto o avatar.  El formulario es muy sencillo, y en la web solo serán visibles tu nombre de usuario, foto y código postal.
+
+### 7. ¿Cómo puedo solicitar alimentos?
+No se publican anuncios para solicitar comida pero puedes ver lo que ofrecen otras personas y contactar con ellas. Puedes hacerlo desde la sección ALIMENTOS PARA COMPARTIR-Entre particulares;  Para ello solo tienes que registrarte y ponerte en contacto con la persona que ofrece los alimentos que te interesan.
+
+### 8. ¿Cuáles son los criterios para compartir alimentos?
+Deben ser alimentos que están en buen estado pero que, por motivos diferentes, crees que no vas a consumir antes de la fecha de caducidad. Si tú no lo comes porque está en mal estado, su destino es la basura, NUNCA la donación o intercambio. Asegúrate de que lo que ofreces está en buen estado en el momento de la entrega, si tienes alguna duda, mejor no lo hagas.
+
+### 9. ¿Cómo me comunico con la persona que ofrece?
+Puedes enviar un mensaje a la persona que ofrece alimentos que quieres, desde la propia aplicación en la web. Para ello deberás registrarte y ponerte en contacto con la persona que ofrece los alimentos que te interesan; lo puedes hacer desde el botón “Contacta” que aparece junto al perfil de la persona que ofrece ese alimento.
+
+### 10. ¿A quién puedo ofrecer la comida?
+Los anuncios de comida que publique cada usuario estarán visibles para cualquier persona que visita la web.  Si quieres ofrecer comida a alguna asociación, la web te ofrece también un buscador de organizaciones que reciben comida para entregárselas a familias sin recursos o con dificultades para cubrir las necesidades básicas de alimentación. Normalmente en estos casos recogen únicamente productos no perecederos, y pocas veces productos frescos. Puedes contactar con ellas y preguntar.
+
+### 11. ¿Puedo utilizar otras vías de comunicación con la persona que ofrece?
+Cualquier otra vía de comunicación deberás acordarla con la persona interesada. Esta web no facilita otras vías de contacto, para respetar la privacidad de cada usuaria/o. 
+
+### 12. ¿Hay criterios de comunicación? 
+* Los anuncios para la oferta de alimentos deben ser claros y comprensibles, y debes subir una foto real de los alimentos que estás ofreciendo, para poder ver su estado.
+
+* Los comentarios y valoraciones que hagamos de las entregas realizadas, y las personas usuarias han de ser respetuosas (aunque sean críticas), sin insultos y procurando aportar soluciones a los problemas que se nos plantean. 
+
+* Por supuesto, publicar también comentarios y felicitaciones a todas las buenas ideas, entregas y tareas que vayamos observando.
+
+* Respetar la privacidad de las personas que participan en la red; no dar a conocer más datos de los que aparecen en la web con carácter público.
+
+* Las recetas de aprovechamiento y trucos que compartamos en IDEAS FRESCAS deben estar escritas en un lenguaje comprensible y sencillo. Intenta escribir frases cortas. Se deberán rellenar todos los campos del formulario y si podemos subir una foto que muestre nuestra idea, mucho más claro y útil. Si utilizas fotos o textos que no son tuyos, por favor, cita la fuente y autoría y asegúrate que las puedes difundir, es importante que respetemos las ideas de los demás. Aquí tienes algunos ejemplos de páginas con <a href="http://blog.hubspot.es/marketing/imagenes-gratis-y-fotos-libres-de-derechos." target="_blank">fotos de uso libre</a>.
+
+* Cuando subas una foto mejor que sea en horizontal, por el espacio que tienen reservado en la web. Está programado para un tamaño de 16:9, que es lo que puedes tener con tu móvil (en horizontal).
+
+### 13. ¿Cómo se realiza el envío?
+La entrega se acuerda entre las personas participantes en el intercambio. Lo normal será que se acuerde un punto de encuentro en un lugar cercano para ambas partes.
+
+### 14. ¿Cómo me mantengo informada de las ofertas? 
+A través de la app y la página web tú puedes ver qué personas están ofreciendo alimentos. Puedes seguirnos también a través de nuestro perfil de <a href="https://www.facebook.com/yonodesperdicio">facebook</a> y <a href="https://twitter.com/nodesperdicio">tuiter</a>.
+
+### 15. ¿Cómo realizo la comprobación de alimentos que me entregan?
+Cuando recojas alimentos que te han entregado, en ese momento, no después..., comprueba que están en buen estado y asegúrate de consumirlos pronto y conservarlos de forma correcta. Revisa la fecha de caducidad y/o consumo preferente y si tienes dudas de que se encuentre en mal estado (olores y apariencia sospechosa) es mejor que no lo consumas.
+
+Te lo recordamos: la fecha de caducidad indica el momento hasta el cual puedes consumir el alimento de forma segura y consumo preferente indica el momento hasta el cual el alimento conserva la calidad prevista. <a href="http://aesan.msssi.gob.es/AESAN/web/rincon_consumidor/subseccion/fechas_caducidad.shtml" target="_blank">Más información</a>. 
+
+### 16. ¿Dónde puedo hacer reclamaciones si la persona no da alimentos en buen estado? 
+Los intercambios son responsabilidad exclusiva de los particulares implicados, en ningún caso de la entidad promotora de esta iniciativa. No obstante, es importante que nos des tu opinión y escribas tu comentario sobre la entrega o recogida realizada para, de esta forma, informar de comportamientos inadecuados y conseguir una confianza y autorregulación que garanticen el buen funcionamiento de esta iniciativa.  Cada perfil tendrá un sistema de valoración para que el resto de usuarios/as pueda valorar las entregas realizadas.
+
+### 17. ¿Dónde veo los datos de la persona que ofrece los alimentos?
+Cada usuario/a registrada podrá ver los datos de las personas que se van sumando a esta iniciativa. Serán datos básicos: nombre (real o inventado para este perfil) y un código postal que permita situar a las personas más cercanas para facilitar las entregas de productos. Si te interesa la oferta de alimentos que hace podrás enviar un mensaje para pedir más información o acordar la entrega.
+
+### 18. ¿Cuáles son los casos en los que no podrás compartir alimentos?
+Salubridad de los alimentos: si tú no lo comes porque está en mal estado, su destino es la basura, NUNCA la donación o intercambio. Asegúrate de que lo que ofreces está en buen estado en el momento de la entrega, si tienes alguna duda, mejor no lo hagas. No ofrezcas productos caducados. 
+
+### 19. ¿Cómo mejoramos la herramienta en comunidad?
+Es importante que nos des tu opinión, valores la oferta o donación, y escribas tu comentario sobre cómo ha sido la entrega de comida. Poco a poco iremos consiguiendo así una valoración y autorregulación de la comunidad. 
+
+
+* Si eres programador/a puedes ver el código y aportar mejoras.
+
+* Si eres comunicador/a puedes ayudar en su difusión, compartiendo en redes sociales u otras formas.
+
+* Si eres una persona que ha dado alimentos puedes mencionar tu experiencia a otros de tu red.
+
+* Si eres usuario/a y ves incidencias puedes reportarlas aquí: cuentame@yonodesperdicio.org
+
+### 20¿Qué clase de anuncios puedo publicar? 
+La filosofía de esta página es reducir el desperdicio de alimentos compartiendo con otras personas aquellos que no vamos a consumir. Puedes publicar anuncios en los que ofreces alimentos a cambio de nada. No son anuncios de intercambio, venta, anuncios comerciales, solicitudes de empleo, medicamentos, etc. Estos anuncios se borrarán por el administrador de la página si se detectan y se puede proceder al bloqueo de la cuenta del usuario/a si se siguen publicando. 
+
+### 21. ¿Qué información es necesaria para el anuncio?
+Cada anunciante deberá describir perfectamente el alimento o alimentos que ofrece: nombre del alimento + foto del estado actual + cantidad/kg + usuario que lo ofrece + código postal de localización. + fecha de caducidad  y fecha límite de entrega. 
+
+Con algunos alimentos puede ocurrir que no sepamos cuál es la fecha de caducidad, por ejemplo, frutas, verduras, etc. pero podemos ponerle una fecha límite de entrega a otra persona, para asegurar que el alimento se entrega en buen estado.
+
+### 22. ¿Dónde funcionan las ofertas y solicitudes? 
+Funcionan en todos los municipios y ciudades es los que haya usuarios/as.  Cuantas más personas se sumen a la iniciativa, más posibilidades de ofertas de alimentos tendremos. Una única persona en una ciudad no tendría muchas posibilidades de entregar los alimentos, pero si animamos a más grupos, personas y somos conscientes de que podemos reducir la problemática del desperdicio de alimentos, iremos extendiendo más esta iniciativa.
+
+### 23. ¿Cómo funciona el contador de comida?
+Cada vez que consigas realizar una entrega de comida a otra personas/organización podemos felicitarnos porque esa comida ya no va a la basura. En este momento, desde tu perfil, marca la casilla de “ENTREGADO” y esos kilos o gramos se sumarán a tu contador personal y al general. De esta forma podemos ver cómo vamos colaborando. "
 Text.create code: "legal", 
             title: "Información legal", 
-            body: "lalala"
+            body: "La asociación Prosalus, con CIF G-78223872 y  domicilio social en la calle María Panés nº 4 (28003 Madrid), es titular de la web site **yonodesperdicio.org** 
+
+Estos términos de uso generales tienen por objeto regular las condiciones y las formas de uso del sitio web  **yonoderperdicio.org**  así como definir los derechos y las obligaciones de las partes implicadas en su utilización.
+
+Todos los contenidos accesibles en la página web **yonodesperdicio** desarrollado por Prosalus están sujetos a licencia Creative Commons.
+
+El código fuente de **yonodesperdicio.org** es código abierto y está disponible para ser utilizado (bajo licencia <a href="http://www.gnu.org/licenses/agpl-3.0.html" target="_blank">Affero GPL 3</a>), y basado en la versión Ruby on Rails de <a href="http://nolotiro.org" target="_blank">Nolotiro</a>. 
+ 
+Se concede el derecho de reproducción del contenido de la página **yonoderperdicio.org** siempre que se respeten las siguientes condiciones:
+
+* Que el fin con que se utilicen sea compatible con los fines de esta iniciativa. 
+* Que la finalidad sea obtener la información contenida en la página y no con propósito comercial. 
+* Que, en caso de reproducción o utilización, se cite la fuente. 
+* Que ninguno de los elementos contenidos en la web tales como datos, documentos, información, gráficos o imágenes, sean modificados de forma alguna.
+
+El contenido de dicha página es responsabilidad exclusiva de Prosalus y no refleja necesariamente la opinión de financiadores o patrocinadores de la misma.
+
+Prosalus no garantiza la inexistencia de errores en el acceso a la Web, en su contenido, ni que éste se encuentre actualizado, aunque desarrollará sus mejores esfuerzos para, en su caso, evitarlos, subsanarlos o actualizarlos. Agradecemos todas las aportaciones que puedas hacernos a este respecto.
+
+Prosalus se reserva la facultad de efectuar, en cualquier momento y sin necesidad de previo aviso, modificaciones y actualizaciones de la información contenida en su web o en la configuración y presentación de ésta.
+
+En cumplimiento de la Ley Orgánica 15/1999, de 13 de Diciembre, de Protección de Datos de Carácter Personal (LOPD), te informamos que no se cederán los datos personales de los usuarios y usuarias de la web sino que se utilizarán exclusivamente con la finalidad para la que se diseña esta iniciativa. 
+
+Prosalus es responsable del fichero “Comunidad”, inscrito en el Registro General de Protección de Datos, y te informamos de que los datos que nos facilites serán incluidos en dicho fichero. Podrás ejercitar tus derechos de acceso, rectificación, cancelación y oposición dirigiéndote por escrito, con una copia de documento acreditativo, a PROSALUS, como titular del fichero.
+
+Como usuario/a garantizas que todos los datos que facilites mediante el formulario de inscripción y/o mediante cualquier otro formulario o medio de contacto facilitado son exactos, están actualizados, son verídicos, son de tu propia titularidad y no tienen ninguna intención fraudulenta.  Asimismo, te comprometes a actualizar tus datos en caso de que haya alguna modificación, para que se correspondan con los criterios anteriores.
+
+# Cookies
+
+Utilizamos cookies para facilitar el uso de nuestra página web. Las cookies son pequeños ficheros de texto que tu navegador almacena en el disco duro de tu ordenador y que son necesarias para utilizar nuestra página web. Las utilizamos para entender mejor la manera en la que se usa nuestra página web y de esta manera poder mejorar consecuentemente el proceso de navegación. Las cookies son el referente que nos indica, por ejemplo, si una página de nuestra web ha sido vista con anterioridad, o si su visita es nueva o recurrente. Las cookies que utilizamos no almacenan datos personal alguno, ni ningún tipo de información que pueda identificarte. En caso de no querer recibir cookies, por favor configura tu navegador de Internet para que las borre del disco duro de tu ordenador, las bloquee o te avise en caso de instalación de las mismas. 
+
+La utilización de esta página, implica la aceptación de utilización de cookies, en los términos y condiciones previstos anteriormente."
 Text.create code: "links", 
             title: "Links intersantes", 
             body: "* [Prosalus](http://prosalus.es)
