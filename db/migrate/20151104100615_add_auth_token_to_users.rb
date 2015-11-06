0149e9a96605ba8eb34a9a35305fb57e53584e46
Amaia Castro <amaia@amaiac.net>
2015-11-06 19:46:09 +0100

    Fix migration

--- db/migrate/20151104100615_add_auth_token_to_users.rb
+++ db/migrate/20151104100615_add_auth_token_to_users.rb
@@ -1,6 +1,6 @@
 class AddAuthTokenToUsers < ActiveRecord::Migration
   def change
-    add_column :users, :auth_token, :string, default: ""
+    add_column :users, :auth_token, :string
     add_index :users, :auth_token, unique: true
   end
 end
