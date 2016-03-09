cdf750049a55ae356db544d90770aba9890b2cb6
Amaia Castro <amaia@amaiac.net>
2016-03-09 19:29:46 +0100

    cleanup

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,3 @@
 # Be sure to restart your server when you modify this file.
 
-Yonodesperdicio::Application.config.session_store :cookie_store, key: '_nolotiro_org_session'
+Yonodesperdicio::Application.config.session_store :cookie_store, key: '_yonodesperdicio_session'
